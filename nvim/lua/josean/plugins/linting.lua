a053a8e57bfa4a65be1aab4b7025578fc5afb062
ayush.sherpa <ayushsherpa@leapfrogtechnology.com>
2026-07-24 23:15:28 +0545

    linters added, formatter for yml commented

--- nvim/lua/josean/plugins/linting.lua
+++ nvim/lua/josean/plugins/linting.lua
@@ -5,7 +5,7 @@ return {
     local lint = require("lint")
 
     lint.linters_by_ft = {
-      -- python = { "pylint" },
+      python = { "pylint" },
       -- javascript = { "eslint" },
       -- typescript = { "eslint" },
     }
